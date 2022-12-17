5b20756b6987664dd1219c96c6c22328c762037e
Sheldon Nofer <snofer@bethel.jw.org>
2022-12-17 14:08:15 -0500

    feat: add code block section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 interface IModel {
-  rootNamespace: string;
+  globalNamespace: string;
   domainNamespace: string;
 }
 
@@ -13,14 +13,43 @@ interface IModel {
 export class AppComponent {
   title = 'DynamoDbEntityBuilder';
   model: IModel = {
-    rootNamespace: '',
+    globalNamespace: '',
     domainNamespace: '',
   };
+  formSubmitted = false;
   pk = '';
   sk = '';
+  pkName = 'pk';
+  skName = 'sk';
+  tableName = 'my-table-name';
+  keySeparator = '#';
+  entityName = 'MyEntity';
+  cSharpModel = `
+using System;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace MyNamespace;
+
+[DynamoDBTable("${this.tableName}", true)]
+public class ${this.entityName}
+{
+    public ${this.entityName}()
+    {
+    }
+
+    [DynamoDBHashKey("${this.pkName}")] 
+    public string ${this.pkName} { get; set; }
+
+    [DynamoDBRangeKey("${this.skName}")] 
+    public string ${this.skName} { get; set; }
+
+    /// Add additional entity properties here as regular CSharp properties
+}
+  `;
 
   generateEntity() {
-    this.pk = `${this.model.rootNamespace}#unique-id`;
-    this.sk = `${this.model.domainNamespace}#object-identifier`;
+    this.formSubmitted = true;
+    this.pk = `${this.model.globalNamespace}${this.keySeparator}unique-id`;
+    this.sk = `${this.model.domainNamespace}${this.keySeparator}object-identifier`;
   }
 }
